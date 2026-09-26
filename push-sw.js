d02aa6238c64a6f242e227b859e9864acc70dc67
jorge2610g <scuentas150@gmail.com>
2026-09-25 23:35:07 -0300

    Refresh Streaming PWA cache after URL fix

--- push-sw.js
+++ push-sw.js
@@ -1,4 +1,4 @@
-const YUMMYPRO_CACHE="yummypro-pwa-v2508-singleapp";
+const YUMMYPRO_CACHE="yummypro-pwa-v2509-staging-public-links";
 const YUMMYPRO_OFFLINE_URL="/offline.html";
 const YUMMYPRO_LANDING_OFFLINE="/restaurant-offline.html";
 const YUMMYPRO_CORE=[YUMMYPRO_OFFLINE_URL,YUMMYPRO_LANDING_OFFLINE,"/manifest.webmanifest","/pwa-icon.svg","/icon-192.png","/icon-512.png","/apple-touch-icon.png"];
